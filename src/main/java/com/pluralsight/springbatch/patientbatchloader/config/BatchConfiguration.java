e9c5363b614cc069c970c5ad7f8e7f6b51660ccd
sidrockzz <marshtomp.1999@hotmail.com>
2021-05-05 16:16:58 +0530

    Added Database objects

--- src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchConfiguration.java
+++ src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchConfiguration.java
@@ -1,21 +1,21 @@
 package com.pluralsight.springbatch.patientbatchloader.config;
 
-
 import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.explore.support.JobExplorerFactoryBean;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.SimpleJobLauncher;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.stereotype.Component;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+
 
 @Component
 @EnableBatchProcessing
@@ -32,9 +32,35 @@ public class BatchConfiguration implements BatchConfigurer {
     @Autowired
     @Qualifier(value = "batchDataSource")
     private DataSource batchDataSource;
-// will handle actual bean configuration
+
     @Override
     public JobRepository getJobRepository() throws Exception {
+        return this.jobRepository;
+    }
+
+    @Override
+    public JobLauncher getJobLauncher() throws Exception {
+        return this.jobLauncher;
+    }
+
+    @Override
+    public JobExplorer getJobExplorer() throws Exception {
+        return this.jobExplorer;
+    }
+
+    @Override
+    public PlatformTransactionManager getTransactionManager() {
+        return this.batchTransactionManager;
+    }
+
+    protected JobLauncher createJobLauncher() throws Exception {
+        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+        jobLauncher.setJobRepository(jobRepository);
+        jobLauncher.afterPropertiesSet();
+        return jobLauncher;
+    }
+
+    protected JobRepository createJobRepository() throws Exception {
         JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
         factory.setDataSource(this.batchDataSource);
         factory.setTransactionManager(getTransactionManager());
@@ -42,27 +68,8 @@ public class BatchConfiguration implements BatchConfigurer {
         return factory.getObject();
     }
 
-    @Override
-    public PlatformTransactionManager getTransactionManager() throws Exception {
-        return null;
-    }
-
-    @Override
-    public JobLauncher getJobLauncher() throws Exception {
-        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
-        jobLauncher.setJobRepository(jobRepository);
-        //Ensures jobrepositories are on set
-        jobLauncher.afterPropertiesSet();
-        return jobLauncher;
-    }
-
-    @Override
-    public JobExplorer getJobExplorer() throws Exception {
-        return null;
-    }
-
     @PostConstruct
-    public void afterPropertiesSet() throws Exception{
+    public void afterPropertiesSet() throws Exception {
         this.jobRepository = createJobRepository();
         JobExplorerFactoryBean jobExplorerFactoryBean = new JobExplorerFactoryBean();
         jobExplorerFactoryBean.setDataSource(this.batchDataSource);
